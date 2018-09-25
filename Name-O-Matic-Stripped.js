e910e318db5095bb9c7af333d7f3a6d28306560e
Thomas Brace <thomas.idris.brace@googlemail.com>
2018-09-25 13:14:56 +0100

    DynamoDB added
    Save names added
    
    to do:
    remove all saved names
    reset filter after letter filter added
    change family name
    add extra names

--- Name-O-Matic-Stripped.js
+++ Name-O-Matic-Stripped.js
@@ -15,13 +15,28 @@ let boysNames = ["Oliver","George","Harry","Jack","Jacob","Noah","Charlie","Muha
 let girlsNames = ["Amelia","Olivia","Emily","Isla","Ava","Jessica","Ella","Isabella","Poppy","Mia","Sophie","Sophia","Lily","Grace","Evie","Scarlett","Ruby","Chloe","Daisy","Isabelle","Phoebe","Florence","Freya","Alice","Charlotte","Sienna","Matilda","Evelyn","Eva","Millie","Sofia","Lucy","Elsie","Imogen","Layla","Rosie","Maya","Elizabeth","Esme","Willow","Lola","Ivy","Holly","Emilia","Molly","Erin","Jasmine","Eliza","Ellie","Abigail","Lilly","Eleanor","Georgia","Hannah","Harriet","Maisie","Amber","Emma","Annabelle","Bella","Amelie","Thea","Harper","Rose","Gracie","Summer","Violet","Martha","Penelope","Anna","Zara","Nancy","Maria","Maryam","Darcie","Darcey","Heidi","Lottie","Megan","Francesca","Mila","Lexi","Bethany","Julia","Lacey","Robyn","Aisha","Victoria","Zoe","Clara","Sara","Beatrice","Darcy","Leah","Arabella","Hollie","Sarah","Maddison","Katie","Eloise"]
 let pickArry = [];
 
+let nameArr = new Array();
+
 // The handlers object tells Alexa how to handle various actions
 let handlers = {
 	'LaunchRequest': function () {
-		//session attributes
-		//this.attributes['familyName'] = "";
+console.log('launch')
+console.log(this.attributes)
+console.log(this.attributes.savedNames)
+ 		if (Object.keys(this.attributes).length === 0) {
+			//fresh login
 
-		this.response.speak(welcomeOutput).listen(welcomeReprompt)
+			this.response
+				.speak("Welcome to the name o matik generator, let me know the surname you would like me to pair with, and if you would like a boys or girls name. Then i'll see what i can come up with")
+				.listen("Would you like a boys or a girls name?, or a name begining with a particular Letter?");
+		}else{
+			//retuning user
+			this.response
+				.speak("Welcome back to the name o matik generator. Your family name is currently set as " + this.attributes.familyName + " and you have " + this.attributes.savedNames.length + " saved names. Shall I generate a new name for you?")
+				.listen("Would you like a boys or a girls name?, or a name begining with a particular Letter? or revew your saved names?");
+		}
+
+
 		this.emit(':responseReady');
 	},
 	'AMAZON.HelpIntent': function () {
@@ -40,10 +55,9 @@ let handlers = {
 		this.response.speak(speechOutput).listen()
 		this.emit(':responseReady');
 	},
-	'SessionEndedRequest': function () {
-		speechOutput = '';
-		this.response.speak(speechOutput).listen()
-		this.emit(':responseReady');
+	'SessionEndedRequest': function() {
+    console.log('session ended!');
+    this.emit(':saveState', true);
 	},
 	'AMAZON.FallbackIntent': function () {
 		speechOutput = "I didn't quite get that, please try again.";
@@ -69,30 +83,40 @@ let handlers = {
 
 		//get slots
 		let sexSlot = this.event.request.intent.slots.sex.value;
-		let lengthSlot = this.event.request.intent.slots.length.value;
+		let lengthSlot = this.event.request.intent.slots.length.value; // not currently used
 		let surnameSlot = this.event.request.intent.slots.surname.value;
 		let numberSlot = this.event.request.intent.slots.number.value;
 		let letterSlot = this.event.request.intent.slots.letter.value;
-		let middleNameSlot = this.event.request.intent.slots.middleName.value;
-		let firstNameSlot = this.event.request.intent.slots.firstName.value;
+		let middleNameSlot = this.event.request.intent.slots.middleName.value; // not currently used
+		let firstNameSlot = this.event.request.intent.slots.firstName.value; // not currently used
 
 
 		//Save the sessions slots if they haven't been updated in the latest requests
+		if (typeof sexSlot != "undefined"){
+			this.attributes['sexSlot'] = sexSlot;
+		}
 		if (typeof surnameSlot != "undefined"){
 			this.attributes['familyName'] = surnameSlot;
 		}
+		// if (typeof numberSlot != "undefined"){
+		// 	this.attributes['numberSlot'] = numberSlot;
+		// 		console.log('__numberSlot:' + this.attributes['numberSlot'])
+		// }
+		if (typeof letterSlot != "undefined"){
+			this.attributes['letterSlot'] = letterSlot;
+		}
 
 		//setup pick list by gender
-		if (typeof sexSlot == "undefined"){
+		if (typeof this.attributes['sexSlot'] == "undefined"){
 			pickArry = boysNames.concat(girlsNames);
-		} else if (sexSlot.toLowerCase() === "boys" || sexSlot.toLowerCase() === "boy") {
+		} else if (this.attributes['sexSlot'].toLowerCase() === "boys" || this.attributes['sexSlot'].toLowerCase() === "boy") {
 			pickArry = boysNames;
 		} else {
 			pickArry = girlsNames;
 		}
 		//filter by letter if required
-		if (typeof letterSlot != "undefined"){
-			pickArry = pickArry.filter((name) => name.startsWith(letterSlot.toUpperCase()));
+		if (typeof this.attributes['letterSlot'] != "undefined"){
+			pickArry = pickArry.filter((name) => name.startsWith(this.attributes['letterSlot'].toUpperCase()));
 		}
 		//get number of names to pick
 		if (typeof numberSlot != "undefined"){
@@ -104,7 +128,8 @@ let handlers = {
 
 			for (var i=0; i<numberSlot; i++){
 				pickedIndex = Math.floor(Math.random() * pickArry.length)
-				pickedNames = pickedNames + pickArry[pickedIndex] + " " + this.attributes['familyName'] + ","
+				this.attributes['currentName'] = pickArry[pickedIndex] + " " + this.attributes['familyName'] //save last name for potential save
+				pickedNames = pickedNames + this.attributes['currentName'] + ", "
 				pickArry.splice(pickedIndex,1)
 			}
 
@@ -114,9 +139,71 @@ let handlers = {
 			// random pick from remaining list
 			let pickedName = pickArry[Math.floor(Math.random() * pickArry.length)];
 			speechOutput = " How's this? " + pickedName + " " + this.attributes['familyName'];
+
+			//save current name for potential save
+			this.attributes['currentName'] = pickedName + " " + this.attributes['familyName'];
 		}
 
 		this.response.speak(speechOutput + ". should I generate another?").listen("should I generate another?");
+		this.emit(':responseReady');
+	},
+	'saveNameIntent': function () {
+
+		//get slots
+		let lastNameSlot = this.event.request.intent.slots.lastName.value; // not currently used
+		let firstNameSlot = this.event.request.intent.slots.firstName.value; // not currently used
+
+		let currentName = "";
+		//Save the sessions slots if they haven't been updated in the latest requests
+		if (typeof firstNameSlot != "undefined"){
+			if (typeof lastNameSlot != "undefined"){
+				currentName = firstNameSlot + " " + lastNameSlot;
+			} else {
+				currentName = firstNameSlot + " " + this.attributes['familyName']
+			}
+			this.attributes['currentName'] = currentName;
+		} else {
+			// assume they are refering to saving most recent name.
+			currentName = this.attributes['currentName'];
+		}
+		nameArr.push(currentName)
+		this.attributes.savedNames = nameArr;
+
+		this.response.speak("I've added " + currentName + " to your saved names").listen("should I generate name?");
+		this.emit(':responseReady');
+	},
+	'reviewNamesIntent': function () {
+		this.response.speak("Your saved names are:" + this.attributes.savedNames).listen("Ask me to remove any names or ask me new ones?");
+		this.emit(':responseReady');
+	},
+	'removeNameIntent': function () {
+
+	let firstNameSlot = this.event.request.intent.slots.firstName.value;
+	let lastNameSlot = this.event.request.intent.slots.lastName.value;
+	let currentName = "";
+
+	//Save the sessions slots if they haven't been updated in the latest requests
+	if (typeof firstNameSlot != "undefined"){
+		if (typeof lastNameSlot != "undefined"){
+			currentName = firstNameSlot + " " + lastNameSlot;
+		} else {
+			currentName = firstNameSlot + " " + this.attributes['familyName']
+		}
+		this.attributes['currentName'] = currentName;
+	} else {
+		// assume they are refering to saving most recent name.
+		currentName = this.attributes['currentName'];
+	}
+	let index = this.attributes.savedNames.indexOf(currentName)
+	//try and find name in saved names
+	if (index != -1) {
+		this.attributes.savedNames.splice(index,1)
+		this.response.speak("I've removed " + currentName + " from your saved names. Would you like me to generate some new names?" ).listen("should I generate name?");
+	} else {
+		this.response.speak("I could not find " + currentName + " in your saved names. Would you like me to generate some new names?" ).listen("should I generate name?");
+	}
+
+
 		this.emit(':responseReady');
 	}
 };
@@ -127,6 +214,8 @@ exports.handler = function(event, context, callback) {
 	// Set up the Alexa object
 	var alexa = Alexa.handler(event, context);
 	alexa.appId = APP_ID;
+	// fire up DynameDB and add table. rememebr to setup IAM and set ececution role on lambda
+	alexa.dynamoDBTableName = 'name-o-matic';
   // Register Handlers
   alexa.registerHandlers(handlers);
 	// Start our Alexa code
